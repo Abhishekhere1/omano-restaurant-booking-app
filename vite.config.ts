6948bc4d84bc679de7d617599fe5811900ba95d1
Abhishek <abhishekkumar834438@gmail.com>
2026-06-01 01:00:06 +0530

    Remove node_modules and configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,24 @@
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import { viteSingleFile } from "vite-plugin-singlefile";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(), viteSingleFile()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
+
+
 import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
@@ -10,6 +31,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/omano-restaurant-booking-app/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
